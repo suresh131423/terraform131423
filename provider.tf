9e80129e0e359aba574c8795b78e755f701ae983
suresh131423 <124440830+suresh131423@users.noreply.github.com>
2023-02-04 18:58:12 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "azurerm" {
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-  }
+    features {}
+    client_id       = var.client_id
+    client_secret   = var.client_secret
+    tenant_id       = var.tenant_id
+    subscription_id = var.subscription_id
 }
